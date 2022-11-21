abb3a980b93e4cf49a45e5e39f92b5507c911b5c
Anastasia <115283704+anadaprojects@users.noreply.github.com>
2022-11-21 20:03:10 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+       int a;
+       char b;
+       float c;
+       double d;
+       byte e;
+       short f;
+       long g;
+       boolean h;
+       System.out.println("int max="+ Integer.MAX_VALUE);
+       System.out.println("int min="+ Integer.MIN_VALUE);
+       System.out.println("char max ="+ Character.MAX_VALUE);
+       System.out.println("char min ="+ Character.MIN_VALUE);
+       System.out.println("float max ="+ Float.MAX_VALUE);
+       System.out.println("float min ="+ Float.MIN_VALUE);
+       System.out.println("double max ="+ Double.MAX_VALUE);
+       System.out.println("double min ="+ Double.MIN_VALUE);
+       System.out.println("byte max ="+ Byte.MAX_VALUE);
+       System.out.println("byte min ="+ Byte.MIN_VALUE);
+       System.out.println("short max ="+ Short.MAX_VALUE);
+       System.out.println("short min ="+ Short.MIN_VALUE);
+       System.out.println("long max ="+ Long.MAX_VALUE);
+       System.out.println("long min ="+ Long.MIN_VALUE);
+       //Тип boolean неможливо перевірити.
+
+
+    }
+}
